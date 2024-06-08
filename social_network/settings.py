d38618fd96dbee620cfac869189aad4189a45464
arvind <97010925+22Aravind18@users.noreply.github.com>
2024-06-08 12:47:16 +0530

    Update settings.py

--- social_network/settings.py
+++ social_network/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4z7*erp)l1!a3&@f%+zi!royq@ta%2*_i-&@g#vt!2(y3#sn7f"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
